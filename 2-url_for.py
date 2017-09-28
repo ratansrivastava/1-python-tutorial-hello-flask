bd0768b56764b7a76345853cb016a6085427e2a7
ratansrivastava <ratan.srivastava885@gmail.com>
2017-09-28 13:03:20 +0530

    Create 2-url_for.py

---
+++ 2-url_for.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/admin/')
+def hello_admin():
+  return "Hello admin"
+  
+ @app.route('/guest/<guest>/')
+ def hello_guest():
+  return "Hello %s " %guest
+  
